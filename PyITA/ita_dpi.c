c57295c589d8fc068c5e9f4b1044558ea865bd79
unknown <abdelaal-git>
2026-04-26 14:14:08 +0100

    fix for cpp

--- PyITA/ita_dpi.c
+++ PyITA/ita_dpi.c
@@ -2,24 +2,44 @@
 #include <Python.h>
 #include <numpy/arrayobject.h>
 
-extern "C" void ita_reference_model(
-    const int* input_data,  int input_size,
-    const int* weight_data, int weight_size,
-    const int* bias_data,   int bias_size,
+// Use proper C linkage (no "C++" extern syntax)
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ita_reference_model(
+    const svLogicVecVal* input_data,  int input_size,
+    const svLogicVecVal* weight_data, int weight_size,
+    const svLogicVecVal* bias_data,   int bias_size,
     int S, int P, int E, int F, int H,
     int N, int M, int WI, int WO,
-    int* output_data, int output_size)
+    svLogicVecVal* output_data, int output_size);
+
+#ifdef __cplusplus
+}
+#endif
+
+void ita_reference_model(
+    const svLogicVecVal* input_data,  int input_size,
+    const svLogicVecVal* weight_data, int weight_size,
+    const svLogicVecVal* bias_data,   int bias_size,
+    int S, int P, int E, int F, int H,
+    int N, int M, int WI, int WO,
+    svLogicVecVal* output_data, int output_size)
 {
     if (!Py_IsInitialized()) {
         Py_Initialize();
         import_array();
     }
 
-    PyObject* pName = PyUnicode_DecodeFSDefault("PyITA.run_reference_model");
+    PyObject* pName = PyUnicode_DecodeFSDefault("run_reference_model");
     PyObject* pModule = PyImport_Import(pName);
     Py_DECREF(pName);
 
-    if (pModule == NULL) { PyErr_Print(); return; }
+    if (pModule == NULL) {
+        PyErr_Print();
+        return;
+    }
 
     PyObject* pFunc = PyObject_GetAttrString(pModule, "run_reference_model");
     if (!pFunc || !PyCallable_Check(pFunc)) {
@@ -28,40 +48,50 @@ extern "C" void ita_reference_model(
         return;
     }
 
-    npy_intp d_in[1] = {input_size};
-    npy_intp d_w[1]  = {weight_size};
-    npy_intp d_b[1]  = {bias_size};
+    // Simple conversion from svLogicVecVal to int (for now)
+    int* in_data  = (int*)malloc(input_size * sizeof(int));
+    int* w_data   = (int*)malloc(weight_size * sizeof(int));
+    int* b_data   = (int*)malloc(bias_size * sizeof(int));
+    int* out_data = (int*)malloc(output_size * sizeof(int));
 
-    PyObject* np_in = PyArray_SimpleNewFromData(1, d_in, NPY_INT32, (void*)input_data);
-    PyObject* np_w  = PyArray_SimpleNewFromData(1, d_w,  NPY_INT32, (void*)weight_data);
-    PyObject* np_b  = PyArray_SimpleNewFromData(1, d_b,  NPY_INT32, (void*)bias_data);
+    for (int i = 0; i < input_size; i++)  in_data[i] = input_data[i].aval;
+    for (int i = 0; i < weight_size; i++) w_data[i]  = weight_data[i].aval;
+    for (int i = 0; i < bias_size; i++)   b_data[i]  = bias_data[i].aval;
 
-    PyObject* args = PyTuple_Pack(12,
-        np_in, np_w, np_b,
-        PyLong_FromLong(S), PyLong_FromLong(P), PyLong_FromLong(E),
-        PyLong_FromLong(F), PyLong_FromLong(H),
-        PyLong_FromLong(N), PyLong_FromLong(M),
-        PyLong_FromLong(WI), PyLong_FromLong(WO));
+    // Build arguments
+    npy_intp dims_in[1] = {input_size};
+    npy_intp dims_w[1]  = {weight_size};
+    npy_intp dims_b[1]  = {bias_size};
+
+    PyObject* np_in = PyArray_SimpleNewFromData(1, dims_in, NPY_INT32, in_data);
+    PyObject* np_w  = PyArray_SimpleNewFromData(1, dims_w,  NPY_INT32, w_data);
+    PyObject* np_b  = PyArray_SimpleNewFromData(1, dims_b,  NPY_INT32, b_data);
+
+    PyObject* args = PyTuple_Pack(12, np_in, np_w, np_b,
+                                  PyLong_FromLong(S), PyLong_FromLong(P),
+                                  PyLong_FromLong(E), PyLong_FromLong(F),
+                                  PyLong_FromLong(H), PyLong_FromLong(N),
+                                  PyLong_FromLong(M), PyLong_FromLong(WI),
+                                  PyLong_FromLong(WO));
 
     PyObject* result = PyObject_CallObject(pFunc, args);
 
-    Py_DECREF(args); Py_DECREF(np_in); Py_DECREF(np_w); Py_DECREF(np_b);
-
-    if (result == NULL) {
-        PyErr_Print();
-        Py_DECREF(pFunc); Py_DECREF(pModule);
-        return;
+    // Copy result back
+    if (result) {
+        PyArrayObject* np_res = (PyArrayObject*)result;
+        int* res_ptr = (int*)PyArray_DATA(np_res);
+        int len = PyArray_DIM(np_res, 0);
+        for (int i = 0; i < output_size && i < len; i++) {
+            output_data[i].aval = res_ptr[i];
+            output_data[i].bval = 0;
+        }
+        Py_DECREF(result);
     }
 
-    PyArrayObject* np_res = (PyArrayObject*)result;
-    int* res_data = (int*)PyArray_DATA(np_res);
-    int len = PyArray_DIM(np_res, 0);
-
-    for (int i = 0; i < output_size && i < len; i++) {
-        output_data[i] = res_data[i];
-    }
-
-    Py_DECREF(result);
+    // Cleanup
+    free(in_data); free(w_data); free(b_data); free(out_data);
+    Py_DECREF(args);
+    Py_DECREF(np_in); Py_DECREF(np_w); Py_DECREF(np_b);
     Py_DECREF(pFunc);
     Py_DECREF(pModule);
 }
